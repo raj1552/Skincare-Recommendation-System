54d8e49b45d7e15025b27c7fffb3f8e9bb7d6b9f
raj1552 <anurajshrestha75@gmail.com>
2025-11-26 09:00:36 +0545

    revert next.config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
